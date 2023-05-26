522f93c919abbdb926fb3f784b2ea286a87a190b
vats-shivam10 <123534161+vats-shivam10@users.noreply.github.com>
2023-05-26 12:06:47 +0530

    Update res.tf

--- res.tf
+++ res.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "sivam" {
   ami                    = var.ami
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.dep.key_name
-  vpc_security_group_ids = ["${aws_security_group.shivam_sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.shivam_secg.id}"]
   tags = {
     Name = "shivam"
   }
