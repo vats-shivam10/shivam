2a87209fa2b59cce398d571466f7a55d061873e2
vats-shivam10 <123534161+vats-shivam10@users.noreply.github.com>
2023-05-26 11:51:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAVSFFSTGGX"
-  secret_key = ""
+  access_key = "AKIAVSFFSTGGSXGMXUQX"
+  secret_key = "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"
 }
 
